d3fa4138b20a97c5ae0931d8fdba3c208c3a2e69
Hansuppi <hannaepe@metropolia.fi>
2021-12-13 22:20:09 +0200

    Backend vk1

---
+++ Week1/server-two/views/pug.js
@@ -0,0 +1,22 @@
+const pug = require('pug');
+
+// Compile the source code
+const compiledFunction = pug.compileFile('template.pug');
+
+// Render a set of data
+console.log(compiledFunction({
+    name: 'Timothy'
+}));
+// "<p>Timothy's Pug source code!</p>"
+
+// Render another set of data
+console.log(compiledFunction({
+    name: 'Forbes'
+}));
+// "<p>Forbes's Pug source code!</p>"
+
+// Compile template.pug, and render a set of data
+console.log(pug.renderFile('template.pug', {
+    name: 'Timothy'
+}));
+// "<p>Timothy's Pug source code!</p>"
